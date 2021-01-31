8e2d6ec368d06aa7e35cf651560449b65405be45
devinhadley <devinhadley4@gmail.com>
2021-01-30 17:37:57 -0800

    Uploading files/code.

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import {useState} from 'react'
+
+function App() {
+    
+const [showAddTask, setShowAddTask] = useState(false)
+const [tasks, setTasks] = useState( // State is immutable.
+[
+    {
+      "id": 1,
+      "text": "Doctors Appointment",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": true
+    },
+    {
+      "id": 2,
+      "text": "Meeting at School",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    }
+  ]
+
+
+)
+// Add Task
+// ... Copies the current object and inserts it.
+
+ const addTask = (task) => {
+
+
+
+  const id = Math.floor(Math.random() * 10000) + 1
+  
+  const newTask = {id, ...task }
+
+  setTasks([...tasks, newTask])
+
+
+ }
+ // Delete Task
+ const deleteTask = (id) => {
+
+
+     setTasks(tasks.filter((task) => task.id !== id))
+
+
+
+ }
+
+const toggleReminder = (id) => {
+
+
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder  } : task) )
+
+
+}
+
+// && Tenery operator without else.
+    return (
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+        {showAddTask && <AddTask onAdd={addTask}/>}
+        {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
+      </div> 
+    );
+}
+
+export default App
+
